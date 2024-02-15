288ba452c2d09cd727e86121a1791bcf13e3f901
christian-gerard <1@christiangerard.me>
2024-02-14 22:18:10 -0700

    Adds header and header buttons

--- src/App.js
+++ src/App.js
@@ -6,24 +6,35 @@ function App() {
   
   return (
   <>
+
+
     <Header></Header>
+    
     <Main></Main>
   </>
   
   )
 }
-
 function Header() {
-
 
   return (
     <header class='Header'>
       <h1>DeadBeat</h1>
+
+      <div>
+        <button>Menu</button>
+
+        <button>Profile</button>
+
+        <button>MixTapes</button>
+
+        <button>New Mixtape</button>
+      
+      </div>
+
     </header>
   )
-
 }
-
 function Main() {
   const spotifyUrl = 'https://api.spotify.com/v1/artists/1r93D0anfnfL4M7tYTce0J?si=sO_8_Bx7SMiUVxfATMWnvg'
   const spotifyClientId = 'c8de28af3e044d1285807313a3fe4ae7'
@@ -72,6 +83,10 @@ function Main() {
     </main>
 
   )
+}
+function SideBar() {
+
+  return 
 }
 
 export default App;
